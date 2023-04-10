07f6c33da5a55ac59e6c7520a37c630e6c6b40d1
poubr <46137915+poubr@users.noreply.github.com>
2023-04-10 16:09:28 +0300

    Update App.js
    Changed from manually listing the info to mapping the component

--- src/components/App.js
+++ src/components/App.js
@@ -3,33 +3,23 @@ import Card from "./Card";
 import contacts from "./contact";
 
 
+function createCard(contacts) {
+    return <Card
+        key={contacts.id}
+        img={contacts.img} 
+        name={contacts.name}
+        phone={contacts.phone}
+        email={contacts.email}
+        info={contacts.info}
+    />
+}
+
 function App() {
     return <div>
         <h1 className="heading">Contact Cards</h1>
-        <Card
-            img={contacts[0].img}
-            name={contacts[0].name}
-            phone={contacts[0].phone}
-            email={contacts[0].email}
-            info={contacts[0].info}
-        />
-        <Card
-            img={contacts[1].img}
-            name={contacts[1].name}
-            phone={contacts[1].phone}
-            email={contacts[1].email}
-            info={contacts[1].info}
-
-        />
-        <Card
-            img={contacts[2].img}
-            name={contacts[2].name}
-            phone={contacts[2].phone}
-            email={contacts[2].email}
-            info={contacts[2].info}
-        />
+        {contacts.map(createCard)}
     </div>                                      
-}
+} 
 
 export default App;
 
